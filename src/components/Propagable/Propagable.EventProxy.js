b66780eec282a9537abd2b3f45e371aead0d1091
Peter Seliger <peter.seliger@xing.com>
2013-11-06 20:40:22 +0100

    [Propagable_EventProxy] Mixin has been augmented by its new [stopImmediatePropagation] method.

--- src/components/Propagable/Propagable.EventProxy.js
+++ src/components/Propagable/Propagable.EventProxy.js
@@ -12,20 +12,25 @@ composable("components.Propagable_EventProxy", function (require, global, intern
     isObject = env_introspective.isObject,
     isString = env_introspective.isString,
 
-    isEvent = function (type) {
+    isEvent = function (type) {       // was: isCurrentTargetEvent
       return (
-        isObject(type.currentTarget)
+      //isObject(type.currentTarget)  // - does only work for connected systems that just rely on "components.Observable_SignalsAndSlots" and do augment their events by [currentTarget].
+
+        isObject(type.target)         // - is more general since it does not distinguish standard event objects from augmented ones that feature [currentTarget].
         && isString(type.type)
       );
     }
   ;
 
-  Mixin = function () { // Mixin.
+  Mixin = function () { // Privileged Mixin.
     if (!isEvent(this)) {
       return;
     }
-    var isstoppropagation = false; // does internally create own state.
+    var isstoppropagation = false; // does internally create own mutable state.
 
+    this.stopImmediatePropagation = function () {
+      isstoppropagation = true
+    };
     this.stopPropagation = function () {
       isstoppropagation = true
     };
@@ -46,8 +51,8 @@ composable("components.Propagable_EventProxy", function (require, global, intern
   [http://closure-compiler.appspot.com/home]
 
 
-- Simple          -   263 byte
-composable("components.Propagable_EventProxy",function(a,e,b){a=b.introspective;var c=a.isObject,d=a.isString;return function(){if(c(this.currentTarget)&&d(this.type)){var a=!1;this.stopPropagation=function(){a=!0};this.isStopPropagation=function(){return a}}}});
+- Simple          -   303 byte
+composable("components.Propagable_EventProxy",function(a,e,b){a=b.introspective;var c=a.isObject,d=a.isString;return function(){if(c(this.target)&&d(this.type)){var a=!1;this.stopImmediatePropagation=function(){a=!0};this.stopPropagation=function(){a=!0};this.isStopPropagation=function(){return a}}}});
 
 
 */
