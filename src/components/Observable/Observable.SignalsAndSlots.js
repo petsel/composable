76c02778940c9903c46f92bffa7bc7c28a189ea8
Peter Seliger <peter.seliger@xing.com>
2013-11-06 20:44:35 +0100

    [Observable_SignalsAndSlots] Mixin has been refactored slightly - improved [evt] processing within [dispatchEvent] method, better internal naming and got rid of comment overhead too.

--- src/components/Observable/Observable.SignalsAndSlots.js
+++ src/components/Observable/Observable.SignalsAndSlots.js
@@ -14,6 +14,7 @@ composable("components.Observable_SignalsAndSlots", function (require, global, i
 
 
   var
+  //Object      = global.Object,
     Array       = global.Array,
   //Date        = global.Date,
 
@@ -56,7 +57,7 @@ composable("components.Observable_SignalsAndSlots", function (require, global, i
       var
         event = this
       ;
-      event.constructor = Event;
+    //event.constructor = Event; // Object;
 
       event.target    = target;
       event.type      = type;
@@ -72,10 +73,10 @@ composable("components.Observable_SignalsAndSlots", function (require, global, i
        *  implementing the [[EventListener]] constructor.
        */
       var
-        defaultEvent = new Event(target, type), // default [Event] object
+        defaultEvent = new Event(target, type),   // default [Event] object
         listener = this
       ;
-      listener.constructor = EventListener;
+      listener.constructor = EventListener;       // Object;
 
       listener.handleEvent = function (evt/*:[string|String|Event-like-Object]*/) { /*:void*/
         if (evt && (typeof evt == "object")) {
@@ -85,7 +86,7 @@ composable("components.Observable_SignalsAndSlots", function (require, global, i
           evt.uuid      = defaultEvent.uuid;      //    [target] by manipulating its event-object-like argument.
         //evt.timeStamp = defaultEvent.timeStamp; //
 
-        } else {
+        } else { // since [dispatchEvent] already prevents stepping into the "else" clause it only will be kept as fallback in case of direct [listener.handleEvent] calls.
 
           evt = {                                 //  - create a [defaultEvent] copy.
             target      : defaultEvent.target,
@@ -118,7 +119,7 @@ composable("components.Observable_SignalsAndSlots", function (require, global, i
         observable/*target*/ = this,
 
 
-        eventMap = {},
+        eventTypeMap = {},
 
                                                             //      - alternative wording taken from the wild ...
         addEventListenerAlias     = "addEventListener",     //  e.g. "on" ............ "bind" ...... "addObserver"
@@ -129,25 +130,25 @@ composable("components.Observable_SignalsAndSlots", function (require, global, i
 
         removeEventListener = function (type/*:[string|String]*/, handler/*:[Function]*/) { /*:[true|false]*/
           var
-            event = eventMap[type],
+            eventType = eventTypeMap[type],
             successfully = false
           ;
-          if (event) {
+          if (eventType) {
             var
-              handlers = event.handlers,
-              listeners = event.listeners,
-              idx = handlers.indexOf(handler)
+              handlerList = eventType.handlerList,
+              listenerList = eventType.listenerList,
+              idx = handlerList.indexOf(handler)
             ;
             if (idx >= 0) {
-              handlers.splice(idx, 1);
-              listeners.splice(idx, 1);
+              handlerList.splice(idx, 1);
+              listenerList.splice(idx, 1);
               successfully = true;
             }
           }
           return successfully;
         },
         hasEventListener = function (type/*:[string|String]*/, handler/*:[Function]*/) { /*:[true|false]*/
-          return ((eventMap[type] || false) && (eventMap[type].handlers.indexOf(handler) >= 0));
+          return ((eventTypeMap[type] || false) && (eventTypeMap[type].handlerList.indexOf(handler) >= 0));
         }
       ;
 
@@ -163,27 +164,27 @@ composable("components.Observable_SignalsAndSlots", function (require, global, i
       //if (type && isString(type) && isCallable(handler)) {
         if (type && isString(type) && isFunction(handler)) {
           var
-            event = eventMap[type],
+            eventType = eventTypeMap[type],
             listener = new EventListener(this, type, handler)
           ;
-          if (event) {
+          if (eventType) {
             var
-              handlers = event.handlers,
-              listeners = event.listeners,
-              idx = handlers.indexOf(handler)
+              handlerList = eventType.handlerList,
+              listenerList = eventType.listenerList,
+              idx = handlerList.indexOf(handler)
             ;
             if (idx == -1) {
-              handlers.push(listener.getHandler());   // in order to store a proper handler reference that later on could be compared to.
-              listeners.push(listener);
+              handlerList.push(listener.getHandler());        // in order to store a proper handler reference that later could be compared to.
+              listenerList.push(listener);
 
               reference = listener;
             } else {
-              reference = listeners[idx];
+              reference = listenerList[idx];
             }
           } else {
-            event = eventMap[type] = {};
-            event.handlers = [listener.getHandler()]; // in order to store a proper handler reference that later on could be compared to.
-            event.listeners = [listener];
+            eventType = eventTypeMap[type] = {};
+            eventType.handlerList = [listener.getHandler()];  // in order to store a proper handler reference that later could be compared to.
+            eventType.listenerList = [listener];
 
             reference = listener;
           }
@@ -229,30 +230,26 @@ composable("components.Observable_SignalsAndSlots", function (require, global, i
       observable[dispatchEventAlias] = function (evt/*:[string|String|Event-like-Object]*/) { /*:[true|false]*/
         var
           successfully = false,
-          type = ((evt && (typeof evt == "object") && isString(evt.type) && evt.type) || (isString(evt) && evt)),
-          event = (type && eventMap[type])
-        ;
-        if (event) {
-          var
-          //handlers = [],                                //  - additional list ...
+          type = (
 
-          //listeners = event.listeners,
-            listeners = (event.listeners && array_from(event.listeners)),
-            len = ((listeners && listeners.length) || 0),
+            (evt && (typeof evt == "object") && isString(evt.type) && evt.type)
+
+            || (isString(evt) && (evt = {type: evt}) && evt.type)
+            || ""
+          ),
+          eventType = (type && eventTypeMap[type])
+        ;
+        if (eventType) {
+          var
+            listenerList = (eventType.listenerList && array_from(eventType.listenerList)),
+            len = ((listenerList && listenerList.length) || 0),
             idx = -1
           ;
           if (len >= 1) {
-          //while (++idx < len) {                         //    ... filled on the fly with
-          //                                              //    all current available
-          //  handlers[idx] = listeners[idx].handleEvent; //    handler references.
-          ////handlers.push(listeners[idx].handleEvent);  //
-          //}                                             //  - thus preventing race condition errors that most probably might
-          //idx = -1;                                     //    occur when the [listeners] array mutates whilst iterating it.
             while (++idx < len) {
 
-            //handlers[idx](evt);                         //  - sticking to the current above described solution.
-              listeners[idx].handleEvent(evt);            //  - solution that has been used before without the
-            }                                             //    additional *caching* of handler references.
+              listenerList[idx].handleEvent(evt);
+            }
             successfully = true;
           }
         }
@@ -260,7 +257,7 @@ composable("components.Observable_SignalsAndSlots", function (require, global, i
       };
 
 
-      /*
+      /**
        *  in order to delete properties that were omitted with
        *  the optional configuration object that will be used
        *  for altering the naming of the [Observable] API methods.
@@ -286,8 +283,8 @@ composable("components.Observable_SignalsAndSlots", function (require, global, i
   [http://closure-compiler.appspot.com/home]
 
 
-- Simple          - 1.802 byte
-composable("components.Observable_SignalsAndSlots",function(k,d,q){k=d.Array;var t=d.Math.random,r=q.introspective,l=r.isFunction,g=r.isString,u=l(k.from)&&k.from||q.helpers.makeArray,v=d.uuid&&l(d.uuid.v4)&&d.uuid.v4||function a(e){return e?(e^16*t()>>e/4).toString(16):([1E7]+-1E3+-4E3+-8E3+-1E11).replace(/[018]/g,a)},s=function(a,e){this.constructor=s;this.target=a;this.type=e;this.uuid=v()},p=function(a,e,g){var c=new s(a,e);this.constructor=p;this.handleEvent=function(a){a&&"object"==typeof a?(a.target=c.target,a.type=c.type,a.uuid=c.uuid):a={target:c.target,type:c.type,uuid:c.uuid};g(a)};this.getType=function(){return e};this.getHandler=function(){return g}};return function(a){a="object"==typeof a&&a||{};var e={},d="addEventListener",c="removeEventListener",m="hasEventListener",n="dispatchEvent",k=function(h,a){var b=e[h],f=!1;if(b){var g=b.handlers,b=b.listeners,c=g.indexOf(a);0<=c&&(g.splice(c,1),b.splice(c,1),f=!0)}return f},d=g(a[d])?a[d]:d,c=g(a[c])?a[c]:c,m=g(a[m])?a[m]:m,n=g(a[n])?a[n]:n;this[d]=function(h,a){var b;if(h&&g(h)&&l(a)){var f=e[h];b=new p(this,h,a);if(f){var c=f.handlers,f=f.listeners,d=c.indexOf(a);-1==d?(c.push(b.getHandler()),f.push(b)):b=f[d]}else f=e[h]={},f.handlers=[b.getHandler()],f.listeners=[b]}return b};this[c]=function(a,c){return g(a)&&l(c)&&k(a,c)||a instanceof p&&k(a.getType(),a.getHandler())||!1};this[m]=function(a,c){var b;if(!(b=g(a)&&l(c)&&(e[a]||!1)&&0<=e[a].handlers.indexOf(c))){if(b=a instanceof p){b=a.getType();var f=a.getHandler();b=(e[b]||!1)&&0<=e[b].handlers.indexOf(f)}b=b||!1}return b};this[n]=function(a){var c=!1,b=a&&"object"==typeof a&&g(a.type)&&a.type||g(a)&&a;if(b=b&&e[b]){var f=(b=b.listeners&&u(b.listeners))&&b.length||0,d=-1;if(1<=f){for(;++d<f;)b[d].handleEvent(a);c=!0}}return c};""in this&&delete this[""]}});
+- Simple          - 1.836 byte
+composable("components.Observable_SignalsAndSlots",function(k,d,q){k=d.Array;var s=d.Math.random,r=q.introspective,l=r.isFunction,g=r.isString,t=l(k.from)&&k.from||q.helpers.makeArray,u=d.uuid&&l(d.uuid.v4)&&d.uuid.v4||function a(e){return e?(e^16*s()>>e/4).toString(16):([1E7]+-1E3+-4E3+-8E3+-1E11).replace(/[018]/g,a)},v=function(a,e){this.target=a;this.type=e;this.uuid=u()},p=function(a,e,g){var c=new v(a,e);this.constructor=p;this.handleEvent=function(a){a&&"object"==typeof a?(a.target=c.target,a.type=c.type,a.uuid=c.uuid):a={target:c.target,type:c.type,uuid:c.uuid};g(a)};this.getType=function(){return e};this.getHandler=function(){return g}};return function(a){a="object"==typeof a&&a||{};var e={},d="addEventListener",c="removeEventListener",m="hasEventListener",n="dispatchEvent",k=function(h,a){var b=e[h],f=!1;if(b){var g=b.handlerList,b=b.listenerList,c=g.indexOf(a);0<=c&&(g.splice(c,1),b.splice(c,1),f=!0)}return f},d=g(a[d])?a[d]:d,c=g(a[c])?a[c]:c,m=g(a[m])?a[m]:m,n=g(a[n])?a[n]:n;this[d]=function(h,a){var b;if(h&&g(h)&&l(a)){var f=e[h];b=new p(this,h,a);if(f){var c=f.handlerList,f=f.listenerList,d=c.indexOf(a);-1==d?(c.push(b.getHandler()),f.push(b)):b=f[d]}else f=e[h]={},f.handlerList=[b.getHandler()],f.listenerList=[b]}return b};this[c]=function(a,c){return g(a)&&l(c)&&k(a,c)||a instanceof p&&k(a.getType(),a.getHandler())||!1};this[m]=function(a,c){var b;if(!(b=g(a)&&l(c)&&(e[a]||!1)&&0<=e[a].handlerList.indexOf(c))){if(b=a instanceof p){b=a.getType();var f=a.getHandler();b=(e[b]||!1)&&0<=e[b].handlerList.indexOf(f)}b=b||!1}return b};this[n]=function(a){var c=!1,b=a&&"object"==typeof a&&g(a.type)&&a.type||g(a)&&(a={type:a})&&a.type||"";if(b=b&&e[b]){var f=(b=b.listenerList&&t(b.listenerList))&&b.length||0,d=-1;if(1<=f){for(;++d<f;)b[d].handleEvent(a);c=!0}}return c};""in this&&delete this[""]}});
 
 
 */
