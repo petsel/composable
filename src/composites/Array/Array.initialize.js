41b80f030b89784394648ce063bca453f0d19852
Peter Seliger <peter.seliger@xing.com>
2014-09-10 08:48:48 +0200

    major bugfix and code improvement for better compression.

--- src/composites/Array/Array.initialize.js
+++ src/composites/Array/Array.initialize.js
@@ -6,22 +6,39 @@ composable(/*"composites.Array_initialize"*/"", function (require, global, inter
   "use strict";
 
 
+//  var
+//    array_initialize = require("components.Enumerable_initializeArray"),
+//
+//
+//    Array = global.Array,
+//
+//
+//    isFunction = internalBaseEnvironment.introspective.isFunction,
+//
+//    hook = {}
+//  ;
+//  array_initialize.call(hook);
+//
+//
+//  !isFunction(Array.initialize) && (Array.initialize = hook.initializeArray);
+//
+//
+////return Array;
+////return Array.initialize;
+
+
   var
-    array_initialize = require("components.Enumerable_initializeArray"),
-
-
     Array = global.Array,
 
-
-    isFunction = internalBaseEnvironment.introspective.isFunction
+    hook = {}
   ;
+  require("components.Enumerable_initializeArray").call(hook);
 
+  !internalBaseEnvironment.introspective.isFunction(Array.initialize) && (Array.initialize = hook.initializeArray);
 
-  !isFunction(Array.initialize) && (Array.initialize = array_initialize.call(Array));
-
-
-//return Array;
-//return Array.initialize;
+//
+////return Array;
+////return Array.initialize;
 
 
 });
@@ -34,8 +51,12 @@ composable(/*"composites.Array_initialize"*/"", function (require, global, inter
   [http://closure-compiler.appspot.com/home]
 
 
-- Simple          -   159 byte
-composable("",function(b,a,c){b=b("components.Enumerable_initializeArray");a=a.Array;c=c.introspective.isFunction;!c(a.initialize)&&(a.initialize=b.call(a))});
+- Simple          -   178 byte
+composable("",function(c,a,d){a=a.Array;var b={};c("components.Enumerable_initializeArray").call(b);!d.introspective.isFunction(a.initialize)&&(a.initialize=b.initializeArray)});
+
+
+- Simple          -   186 byte
+composable("",function(b,a,c){b=b("components.Enumerable_initializeArray");a=a.Array;c=c.introspective.isFunction;var d={};b.call(d);!c(a.initialize)&&(a.initialize=d.initializeArray)});
 
 
 */
